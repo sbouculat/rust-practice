0a8743ced1f1e423754047a7e87837f3611a5dbb
Simon Bouculat <simon@bouculat.fr>
2024-03-04 18:26:15 +0100

    Read index from command line + cleanup

--- fibo/src/main.rs
+++ fibo/src/main.rs
@@ -1,11 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("{}", fibo(16));
+    let args: Vec<String> = env::args().collect();
+    let value = args[1].parse::<i32>().unwrap();
+
+    println!("F{} = {}", value, fibo(value));
 }
 
 fn fibo(n: i32) -> i32 {
     match n {
         0 => 0,
         1 => 1,
-        _ => (fibo(n-2) + fibo(n-1)),
+        _ => fibo(n-2) + fibo(n-1),
     }
 }
